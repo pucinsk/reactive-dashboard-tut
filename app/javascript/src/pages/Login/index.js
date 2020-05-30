2741961c7881205d0520f70815a37651b9bda348
Jokūbas Pučinskas <j.pucinskas@gmail.com>
2020-05-30 23:56:06 +0300

    Add login form

--- app/javascript/src/pages/Login/index.js
+++ app/javascript/src/pages/Login/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const Login = () => (
-  <div>Login Page</div>
+  <>
+    <p>Login</p>
+    <form>
+      <label htmlFor="username">Username </label>
+      <input name="username" autoComplete="true" type="text" />
+      <br />
+      <label htmlFor="password">Password</label>
+      <input name="password" autoComplete="true" type="password" />
+      <br />
+      <input type="submit" value="Login" />
+    </form>
+  </>
 )
 
 export default Login
