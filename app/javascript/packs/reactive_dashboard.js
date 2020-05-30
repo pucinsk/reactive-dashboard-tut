c15fb87adfe1d646115c13c9755e41504378989c
Jokūbas Pučinskas <j.pucinskas@gmail.com>
2020-05-30 23:24:36 +0300

    Add todo lists dashboard

--- app/javascript/packs/reactive_dashboard.js
+++ app/javascript/packs/reactive_dashboard.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { createContext, useState, useEffect, useContext } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Dashboard from '../src/pages/Dashoard'
 import Login from '../src/pages/Login'
 import { ApolloProvider } from '../src/Apollo'
+
+import '../src/styles.css'
 
 const DashboardRoutes = () => (
   <>
@@ -12,17 +14,70 @@ const DashboardRoutes = () => (
   </>
 )
 
+export const AppContext = createContext()
+
+const Loader = () => {
+  const { appLoading } = useContext(AppContext)
+
+  if (!appLoading) return null
+
+  return (
+    <div className="loader">
+      <p>Loading...</p>
+    </div>
+  )
+}
+
+const Error = () => {
+  const { appError } = useContext(AppContext)
+
+  if (!appError) return null
+
+  console.error(appError)
+
+  return (
+    <div className="error">
+      <p>Error...</p>
+    </div>
+  )
+}
+
+const AppProvider = ({ children }) => {
+  const [appLoading, setAppLoading] = useState(false)
+  const [appError, setAppError] = useState('')
+
+  return (
+    <AppContext.Provider
+      value={{
+        appLoading,
+        setAppLoading,
+        appError,
+        setAppError
+      }}
+    >
+      <Loader />
+      <Error />
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export default AppContext
+
 const ReactiveDashboard = () => (
   <ApolloProvider>
-    <Router>
-      <ul>
-        <li><Link to='/'>Reactive Dashboard</Link></li>
-        <li><Link to='login'>Login</Link></li>
-      </ul>
-      <div>
-        <DashboardRoutes />
-      </div>
-    </Router>
+    <AppProvider>
+      <Router>
+        <ul>
+          <li><Link to='/'>Reactive Dashboard</Link></li>
+          <li><Link to='login'>Login</Link></li>
+        </ul>
+        <hr />
+        <div>
+          <DashboardRoutes />
+        </div>
+      </Router>
+    </AppProvider>
   </ApolloProvider>
 )
 
--- app/javascript/src/pages/Dashoard/index.js
+++ app/javascript/src/pages/Dashoard/index.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { useQuery, gql } from '@apollo/client'
+import AppContext from '../../../packs/reactive_dashboard'
+
+const TODO_LISTS_QUERY = gql`
+  query getTodoLists{
+    todoLists {
+      id
+      title
+      isDone
+      account{
+        username
+      }
+    }
+  }
+`
+
+const TodoLists = () => {
+  const { setAppLoading, setAppError } = useContext(AppContext)
+  const [todoLists, setTodoLists] = useState([])
+  const { loading, error } = useQuery(
+    TODO_LISTS_QUERY, {
+    onCompleted({ todoLists }) {
+      setTodoLists(todoLists)
+    }
+  })
+
+  useEffect(() => { setAppLoading(loading) }, [loading])
+  useEffect(() => { setAppError(error) }, [error])
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th />
+            <th>List title</th>
+            <th>Done</th>
+            <th>Belongs to</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            todoLists.map(({ id, title, isDone, account: { username } }, idx) => (
+              <tr key={id}>
+                <td>{idx + 1}. </td>
+                <td>{title}</td>
+                <td>{isDone ? 'Yes' : 'No'}</td>
+                <td>{username}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 const Dashboard = () => (
-  <div>Dashboard Page</div>
+  <TodoLists />
 )
 
 export default Dashboard
